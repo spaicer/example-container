453f33fe86b9a56a50684dc17f13247b45815500
ArzelaAscoIi <kristof.herrmann@rwth-aachen.de>
2021-05-22 12:45:49 +0200

    added sentiment analysis example

---
+++ node/sentiment/README.md
@@ -0,0 +1,60 @@
+
+<p align="center">
+    A <a href="https://nodejs.org/en/">Node.js</a> based, dockerized spaicer example container.
+</p>
+
+
+
+## Install
+
+### Prerequisits
+You need to install <a href="https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04">docker</a> and <a href="">docker-compose</a> for the system to work. **Dont forget to set the docker command to work withou sudo.**
+
+### Build and Run docs and srs container
+
+```sh
+docker-compose up
+```
+Executing this command will start two docker container. ReDoc and the application itself. Redoc lets serves a documentation page of the openAPI specification.
+![image](https://user-images.githubusercontent.com/37148029/118663163-e19b0880-b7f0-11eb-8e3c-1d9eba33d3ea.png)
+
+
+
+
+### Build and Run single containers using Docker
+Build the container by 
+```
+docker build -t spaicer-example-container . 
+```
+Run the container 
+```
+docker run -dp 3001:3001 spaicer-example-container
+```
+
+Test the server using curl
+```sh
+curl -X POST http://<your-ip>:3001/example -H "Content-Type: application/json" -d "{\"temperature\":10.4}"
+```
+
+
+### Development 
+
+Install dependencies by 
+```sh
+npm install
+```
+and start the application by 
+
+```sh
+npm run start
+```
+
+### Build the docs 
+Install dependencies by 
+```sh
+npm install
+```
+and run the docs
+```sh
+npm run docs
+```
---
+++ node/sentiment/app.js
@@ -0,0 +1,58 @@
+const debug = require('debug')('app')
+const OpenApiValidator = require('express-openapi-validator');
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser');
+
+
+(async () => {
+
+
+    const port = process.env.PORT || 3001
+    const server = express()
+    server.use(bodyParser())
+
+    //cors
+    var corsOptionsDelegate = function (req, callback) {
+        var corsOptions = {
+            "origin": "*",
+            "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+            "preflightContinue": false,
+            "optionsSuccessStatus": 204
+        }
+        callback(null, corsOptions) // callback expects two parameters: error and options
+    }
+    server.use(cors(corsOptionsDelegate))
+
+    //openapi definitions
+    server.use(
+        OpenApiValidator.middleware({
+            apiSpec: './openapi.yaml',
+            validateRequests: true,
+            validateResponses: true,
+            formats: require('./api/utils/formats'),
+            validateSecurity: {
+                handlers: require('./api/utils/authentication')
+            },
+            $refParser: {
+                mode: 'dereference'
+            }
+        }),
+    );
+
+
+    server.use('/picks', require("./src/http/picks"))
+    server.use('/trend', require("./src/http/trend"))
+
+
+    server.listen(port, () => debug(`Node listening on port ${port}`))
+
+    server.use((err, req, res, next) => {
+        // format error
+        res.status(err.status || 500).json({
+            message: err.message,
+            errors: err.errors,
+        });
+
+    });
+})()
